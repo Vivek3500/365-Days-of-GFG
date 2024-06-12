ac4f1a1a9d2fef19409d72acd9803785868e9f2e
| Vìv¢k Kumàr Mìshrå | <121348801+Vivek3500@users.noreply.github.com>
2024-06-13 00:36:18 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Padovan Sequence/padovan-sequence.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    
+    const int MOD = 1e9 + 7 ;
+    int padovanSequence(int n)
+    {
+       //code here
+       vector<int>dp(n+1 , 0);
+       dp[0] = dp[1] = dp[2] = 1;
+       
+       for(int i = 3 ; i<=n ; i++){
+           dp[i] = (dp[i-2]%MOD + dp[i-3]%MOD) % MOD;
+           
+       }
+       
+       return dp[n] ;
+    }
+    
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	int n;
+    	cin>>n;
+    	Solution ob;
+    	cout<<ob.padovanSequence(n)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
