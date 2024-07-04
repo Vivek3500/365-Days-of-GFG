ebcb303d6be03da39f0acdfd12489137108148d6
| Vìv¢k Kumàr Mìshrå | <121348801+Vivek3500@users.noreply.github.com>
2024-07-04 20:58:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Duplicate Subtrees/duplicate-subtrees.cpp
@@ -0,0 +1,171 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating veactor of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = "";
+        currVal += ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+void preorder(Node* root, vector<int>& temp) {
+    if (!root) {
+        return;
+    }
+    // cout<<root->data<<" ";
+    temp.push_back(root->data);
+    preorder(root->left, temp);
+    preorder(root->right, temp);
+}
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    string helper(Node* root, vector<Node*>& ans, unordered_map<string, int>& m) {
+        if (!root) {
+            return "# "; // Return a special character to represent a null node
+        }
+
+        string node = to_string(root->data) +
+                      " "; // Convert the data of the current node to a string
+
+        // Recursively generate the string representation of the left and right subtrees
+        node += helper(root->left, ans, m);
+        node += helper(root->right, ans, m);
+
+        // Check if the current subtree is a duplicate
+        if (m.count(node) && m[node] == 1) {
+            ans.push_back(
+                root); // Add the root of the duplicate subtree to the result vector
+            m[node]++; // Increase the count of the duplicate subtree
+        } else if (!m.count(node)) {
+            m[node] = 1; // Insert the string representation of the subtree into the
+                         // unordered map with a count of 1
+        }
+
+        return node; // Return the string representation of the current subtree
+    }
+
+    // Function to find and return all the duplicate subtrees in the given binary tree
+    vector<Node*> printAllDups(Node* root) {
+        vector<Node*> ans; // Vector to store the roots of all the duplicate subtrees
+        unordered_map<string, int>
+            m; // Unordered map to keep track of the string representations of subtrees
+
+        // Call the helper function to recursively generate the string representations
+        // of all subtrees and find the duplicates
+        helper(root, ans, m);
+
+        return ans; // Return the vector of duplicate subtrees
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int tc;
+    cin >> tc;
+    while (tc--) {
+
+        string treeString;
+        getline(cin >> ws, treeString);
+
+        Node* root = buildTree(treeString);
+        Solution obj;
+        vector<Node*> res = obj.printAllDups(root);
+
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < res.size(); i++) {
+            vector<int> temp;
+            preorder(res[i], temp);
+            ans.push_back(temp);
+        }
+
+        sort(ans.begin(), ans.end());
+
+        for (int i = 0; i < ans.size(); i++) {
+            for (int j = 0; j < ans[i].size(); j++) {
+                cout << ans[i][j] << " ";
+            }
+            cout << "\n";
+        }
+        // cout<<"\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
