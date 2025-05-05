97fb9bed5e40c23467cff64df7a42d1578d64ecf
| Vìv¢k Kumàr Mìshrå | <121348801+Vivek3500@users.noreply.github.com>
2025-05-05 12:24:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in an almost Sorted Array/search-in-an-almost-sorted-array.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+class Solution {
+  public:
+    int findTarget(vector<int>& arr, int target) {
+        // code here
+        int n = arr.size();
+        int l = 0;
+        int h = n-1;
+        while (l<=h){
+            int mid = l + (h - l) / 2;
+            if (arr[mid] == target) return mid;
+            if (mid > l && arr[mid - 1] == target) return mid - 1;
+            if (mid < h && arr[mid + 1] == target) return mid + 1;
+            if (arr[mid] < target ){
+                l = mid+2;
+            }
+            else {h = mid - 2;}
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t-- > 0) {
+        string str;
+        getline(cin, str);
+
+        stringstream ss(str);
+        vector<int> arr;
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+
+        int target;
+        cin >> target;
+        cin.ignore();
+        Solution sln;
+        int ans = sln.findTarget(arr, target);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
